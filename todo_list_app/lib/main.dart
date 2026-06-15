e39edb3b58f498471f333a4bcbb0f17efab5cf40
Surya Agung Firdaus <ellyglobal29@gmail.com>
2026-06-15 18:33:31 +0700

    feat: membuat fungsi untuk tambah list dan popup input

--- todo_list_app/lib/main.dart
+++ todo_list_app/lib/main.dart
@@ -31,11 +31,9 @@ class TodoListPage extends StatefulWidget {
   State<TodoListPage> createState() => _TodoListPageState();
 }
 
-// halaman pertama aplikasi
+// membuat todo list dinamis
 class _TodoListPageState extends State<TodoListPage> {
-  // const TodoListPage({super.key});
-
-  // membuat tempat untuk menyimpan list sementara
+  // membuat tempat meyimpan data list sementara
   List<String> todoList = [
     'Belajar Flutter',
     'Membuat Aplikasi Todo',
@@ -44,9 +42,60 @@ class _TodoListPageState extends State<TodoListPage> {
     'Deploy Aplikasi',
   ];
 
+  // controller untuk mengambil teks yang di inputkan
+  final TextEditingController _taskController = TextEditingController();
+
+  // fungsi untuk memunculkan popup dialog
+  void _tambahList() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Tambah List Baru'),
+          content: TextField(
+            // hubungkan controller ke text field
+            controller: _taskController,
+            decoration: const InputDecoration(hintText: 'Masukkan List Baru: '),
+          ),
+          actions: [
+            // tombol batal
+            TextButton(
+              // Perintah untuk menutup Dialog
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Batal'),
+            ),
+
+            // tombol simpan
+            TextButton(
+              onPressed: () {
+                // Mengecek agar tidak menyimpan tugas kosong
+                if (_taskController.text.isNotEmpty) {
+                  // setState() sangat penting! Ini memberitahu Flutter bahwa
+                  // ada data yang berubah dan layar harus digambar ulang.
+                  setState(() {
+                    // Tambahkan teks ke array list
+                    todoList.add(_taskController.text);
+                  });
+                  // Bersihkan inputan untuk pemakaian berikutnya
+                  _taskController.clear();
+                  // Tutup Dialog
+                  Navigator.of(context).pop();
+                }
+              },
+              child: const Text('Simpan'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  // fungsi untuk menampilkan isi
   @override
   Widget build(BuildContext context) {
-    // Scaffold adalah struktur dasar halaman (layar putih kosong)
+    // menggunakan sccafold
     return Scaffold(
       // appbar
       appBar: AppBar(
@@ -55,38 +104,83 @@ class _TodoListPageState extends State<TodoListPage> {
       ),
 
       // body
-      // body: const Center(
-      //   // Center digunakan untuk menengahkan widget di dalamnya
-      //   child: Text('Belum ada List Hari ini!', style: TextStyle(fontSize: 20)),
-      // ),
-
-      // mengganti center dan text dengan listView.builder
       body: ListView.builder(
-        // menghitung berapa jumlah list yang ada
         itemCount: todoList.length,
-
-        // itemBuilder akan dipanggil berkali-kali sebanyak jumlah list yang ada (5 kali)
         itemBuilder: (context, index) {
-          // ListTile adalah widget bawaan untuk baris daftar yang rapi
           return ListTile(
-            // ikon kotak kosong dikiri
             leading: const Icon(Icons.check_box_outline_blank),
-            // menampilkan teks sesuai urutan array
             title: Text(todoList[index]),
           );
         },
       ),
 
-      // floating button
+      // button action
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // aksi ketika tombol ditekan (via terminal)
-          print("Tombol Tambah Ditekan!");
-        },
+        // panggil fungsi _tambahList() ketika ditekan
+        onPressed: _tambahList,
         tooltip: 'Tambah List',
-        // menampilkan icon plus
         child: const Icon(Icons.add),
       ),
     );
   }
 }
+
+// halaman pertama aplikasi
+// class _TodoListPageState extends State<TodoListPage> {
+//   // const TodoListPage({super.key});
+
+//   // membuat tempat untuk menyimpan list sementara
+//   List<String> todoList = [
+//     'Belajar Flutter',
+//     'Membuat Aplikasi Todo',
+//     'Test Aplikasi',
+//     'Debug Aplikasi',
+//     'Deploy Aplikasi',
+//   ];
+
+//   @override
+//   Widget build(BuildContext context) {
+//     // Scaffold adalah struktur dasar halaman (layar putih kosong)
+//     return Scaffold(
+//       // appbar
+//       appBar: AppBar(
+//         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+//         title: const Text('My Todo List'),
+//       ),
+
+//       // body
+//       // body: const Center(
+//       //   // Center digunakan untuk menengahkan widget di dalamnya
+//       //   child: Text('Belum ada List Hari ini!', style: TextStyle(fontSize: 20)),
+//       // ),
+
+//       // mengganti center dan text dengan listView.builder
+//       body: ListView.builder(
+//         // menghitung berapa jumlah list yang ada
+//         itemCount: todoList.length,
+
+//         // itemBuilder akan dipanggil berkali-kali sebanyak jumlah list yang ada (5 kali)
+//         itemBuilder: (context, index) {
+//           // ListTile adalah widget bawaan untuk baris daftar yang rapi
+//           return ListTile(
+//             // ikon kotak kosong dikiri
+//             leading: const Icon(Icons.check_box_outline_blank),
+//             // menampilkan teks sesuai dengan urutan array
+//             title: Text(todoList[index]),
+//           );
+//         },
+//       ),
+
+//       // floating button
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: () {
+//           // aksi ketika tombol ditekan (via terminal)
+//           print("Tombol Tambah Ditekan!");
+//         },
+//         tooltip: 'Tambah List',
+//         // menampilkan icon plus
+//         child: const Icon(Icons.add),
+//       ),
+//     );
+//   }
+// }
