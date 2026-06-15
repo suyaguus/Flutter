de50fcdd82c86718647960ad268a00eb77e7afbc
Surya Agung Firdaus <ellyglobal29@gmail.com>
2026-06-15 17:54:32 +0700

    feat: membuat todo list statis

--- todo_list_app/lib/main.dart
+++ todo_list_app/lib/main.dart
@@ -24,9 +24,25 @@ class MyApp extends StatelessWidget {
   }
 }
 
-// halaman pertama aplikasi
-class TodoListPage extends StatelessWidget {
+// mmebuat statefulwidget
+class TodoListPage extends StatefulWidget {
   const TodoListPage({super.key});
+  @override
+  State<TodoListPage> createState() => _TodoListPageState();
+}
+
+// halaman pertama aplikasi
+class _TodoListPageState extends State<TodoListPage> {
+  // const TodoListPage({super.key});
+
+  // membuat tempat untuk menyimpan list sementara
+  List<String> todoList = [
+    'Belajar Flutter',
+    'Membuat Aplikasi Todo',
+    'Test Aplikasi',
+    'Debug Aplikasi',
+    'Deploy Aplikasi',
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -37,11 +53,30 @@ class TodoListPage extends StatelessWidget {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text('My Todo List'),
       ),
+
       // body
-      body: const Center(
-        // Center digunakan untuk menengahkan widget di dalamnya
-        child: Text('Belum ada List Hari ini!', style: TextStyle(fontSize: 20)),
+      // body: const Center(
+      //   // Center digunakan untuk menengahkan widget di dalamnya
+      //   child: Text('Belum ada List Hari ini!', style: TextStyle(fontSize: 20)),
+      // ),
+
+      // mengganti center dan text dengan listView.builder
+      body: ListView.builder(
+        // menghitung berapa jumlah list yang ada
+        itemCount: todoList.length,
+
+        // itemBuilder akan dipanggil berkali-kali sebanyak jumlah list yang ada (5 kali)
+        itemBuilder: (context, index) {
+          // ListTile adalah widget bawaan untuk baris daftar yang rapi
+          return ListTile(
+            // ikon kotak kosong dikiri
+            leading: const Icon(Icons.check_box_outline_blank),
+            // menampilkan teks sesuai urutan array
+            title: Text(todoList[index]),
+          );
+        },
       ),
+
       // floating button
       floatingActionButton: FloatingActionButton(
         onPressed: () {
